7902999a46745fdc53314c81bcfc220683c243c4
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-02-15 23:32:57 +0530

    Update getRestuarntData.js

--- getRestuarntData.js
+++ getRestuarntData.js
@@ -1,26 +1,23 @@
 const puppeteer = require('puppeteer');
 const {getDataOfEachRestuarnt} = require('./getDataOfEachRestuarnt')
 const {getRestuarntsLinks} = require('./getRestuarntsLinks')
+const {mailer} = require('./mailer')
 
-
-async function getRestuarntData(city, restaurants, maximum = 150, minimum = 100, q) {
+async function getRestuarntData(city, restaurants, maximum = 150, minimum = 100, q,email) {
   const allLinks = await getRestuarntsLinks(city, restaurants);
-  console.log(allLinks)
+
   // make it true for deployment
-  const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"] });
+  const browser = await puppeteer.launch({ headless: false, args: ["--no-sandbox"] });
   let page = await browser.newPage()
   let restuarntsAndDishes = " ";
     
-
-  //   // remove this line
-  // console.log(" the value of q in getRestuarntData  :" + q);
-
   for (let link of allLinks.links) {
-    // removed await for deployement in render
-    restuarntsAndDishes += getDataOfEachRestuarnt(link, page, allLinks.filteredRestuarnt, maximum, minimum, q,restuarntsAndDishes)
+    restuarntsAndDishes += await getDataOfEachRestuarnt(link, page, allLinks.filteredRestuarnt, maximum, minimum, q,restuarntsAndDishes)
   }
 
-  await browser.close()
+   await browser.close()
+   
+   mailer(email, restuarntsAndDishes)
    return  restuarntsAndDishes;
 }
 
