23a2c2215d4791eecf8d3c723fbeb0f0d570539f
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-02-17 13:59:30 +0530

    Update getRestuarntsLinks.js
    accesing Around 45 resturants

--- getRestuarntsLinks.js
+++ getRestuarntsLinks.js
@@ -6,24 +6,36 @@ async function getRestuarntsLinks(city,restaurants)  {
     let page = await browser.newPage();
      page.setDefaultNavigationTimeout(0);
      let filteredRestuarnt = []
+      let links;
 
+
+    let i=0;
+     for (i=0;i<2;i++){
 
     // Navigate to Swiggy website   
     await page.goto(`https://swiggy.com/city/bangalore`);
 
-    let links = await page.$$eval('#restaurants_container div div a',allLinks=>allLinks.map(a=>a.href))
+    let currentLinks = await page.$$eval('#restaurants_container div div a',allLinks=>allLinks.map(a=>a.href))
+     if(i >=1){
+             links = links.concat(currentLinks)
+        }else{
+            links = currentLinks
+        }
     let allRestuarnts =await page.$$eval('#restaurants_container div div a div div:nth-child(2) div:nth-child(1)',res=>res.map(data=>data.innerText));
     
+       if(restaurants && restaurants.length >0){
+            filteredRestuarnt = allRestuarnts.filter(res => restaurants.includes(res)) 
+        }
 
-    if(isNaN(restaurants)){
-        browser.close();
-        return {links,filteredRestuarnt};
-    }
+    //if(isNaN(restaurants)){
+      //  browser.close();
+       // return {links,filteredRestuarnt};
+    //}
 
 
-    if(allRestuarnts && restaurants.length > 0 && typeof(allRestuarnts) != 'undefined' && typeof(allRestuarnts) != 'null')
-        filteredRestuarnt = allRestuarnts.filter(res => restaurants.includes(res)) 
-
+    //if(allRestuarnts && restaurants.length > 0 && typeof(allRestuarnts) != 'undefined' && typeof(allRestuarnts) != 'null')
+        //filteredRestuarnt = allRestuarnts.filter(res => restaurants.includes(res)) 
+     }
     browser.close();
       coonsole.log(links);
     return {links,filteredRestuarnt};
