13339d305e5636a0fe71a0e3c3d1a8f5cbbe3bee
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-04-18 18:06:23 +0530

    Update getRestuarntsLinks.js
    changed from next_button logic to load more logic

--- getRestuarntsLinks.js
+++ getRestuarntsLinks.js
@@ -1,43 +1,47 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-async function getRestuarntsLinks(city,restaurants)  {
-      // make it true for deployment
-    const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox"]});
-    let page = await browser.newPage();
-     page.setDefaultNavigationTimeout(0);
-     let filteredRestuarnt = []
-      let links;
+async function getRestuarntsLinks(city, restaurants) {
+  // make it true for deployment
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"],
+  });
+  let page = await browser.newPage();
+  let filteredRestuarnt = [];
+  let links;
+  let i = 0;
 
+  page.setDefaultNavigationTimeout(0);
+  if (city) await page.goto(`https://swiggy.com/city/${city}`);
+  else await page.goto(`https://www.swiggy.com/city/bangalore`);
 
-    let i=0;
-     for (i=0;i<3;i++){
+  while (i < 10) {
+    await page.$eval(
+      ".RestaurantList__ShowMoreContainer-sc-1d3nl43-0.ikntVJ > div > div:nth-child(1)",
+      (el) => el.click()
+    );
+    await new Promise((r) => setTimeout(r, 2000));
+    i++;
+  }
 
-    // Navigate to Swiggy website   
-    await page.goto(`https://swiggy.com/city/bangalore`);
+  links = await page.$$eval(".sc-iBdmCd.hPntbc > div > a", (allLinks) =>
+    allLinks.map((a) => a.href)
+  );
+  let allRestuarnts = await page.$$eval(
+    ".sc-iBdmCd.hPntbc > div .sc-dmyDGi.dpnlFb",
+    (res) => res.map((data) => data.innerText)
+  );
+  console.log(allRestuarnts);
+  console.log(links);
 
-    let currentLinks = await page.$$eval('#restaurants_container div div a',allLinks=>allLinks.map(a=>a.href))
-     if(i >=1){
-             links = links.concat(currentLinks)
-        }else{
-            links = currentLinks
-        }
-    let allRestuarnts =await page.$$eval('#restaurants_container div div a div div:nth-child(2) div:nth-child(1)',res=>res.map(data=>data.innerText));
-    
-       if(restaurants && restaurants.length >0){
-            filteredRestuarnt = allRestuarnts.filter(res => restaurants.includes(res)) 
-        }
+  if (restaurants && restaurants.length > 0) {
+    filteredRestuarnt = allRestuarnts.filter((res) =>
+      restaurants.includes(res)
+    );
+  }
 
-    //if(isNaN(restaurants)){
-      //  browser.close();
-       // return {links,filteredRestuarnt};
-    //}
+  browser.close();
+  return { links, filteredRestuarnt };
+}
 
-
-    //if(allRestuarnts && restaurants.length > 0 && typeof(allRestuarnts) != 'undefined' && typeof(allRestuarnts) != 'null')
-        //filteredRestuarnt = allRestuarnts.filter(res => restaurants.includes(res)) 
-     }
-    browser.close();
-    return {links,filteredRestuarnt};
-};
-
-module.exports = {getRestuarntsLinks}
+module.exports = { getRestuarntsLinks };
