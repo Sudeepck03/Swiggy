e0bc4f3e5dc4b25c7b8213601998fb8c401e0b78
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-04-18 18:08:42 +0530

    Update SwiggyScrapper.js
    changes made after Swiggy.com UI was changed

--- SwiggyScrapper.js
+++ SwiggyScrapper.js
@@ -1,31 +1,25 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const { getRestuarntData} = require('./getRestuarntData')
+const { getRestuarntData } = require("./getRestuarntData");
 
+app.get("/", (req, res) => {
+  res.send("Swiggy Restuarnt Webscrapping WebSite");
+});
 
-app.get('/', (req, res) => {
-  console.log("/ is the Route");
-  res.send("Swiggy Restuarnt Webscrapping WebSite")
-})
+app.get("/dishes", (req, res) => {
+  try {
+    let { city, restaurants, email, maximum, minimum, q } = req.query;
+    
+    // remove await here for deployment
+    getRestuarntData(city, restaurants, maximum, minimum, q, email);
 
-
-app.get('/dishes',(req,res)=>{
-    try {
-            let { city, restaurants, email, maximum, minimum, q } = req.query
-          console.log("email : " +  email);
-        
-      console.log("dishes");
-        // removw await here for deployment
-         getRestuarntData(city, restaurants, maximum, minimum, q,email);
-            
-            res.send(`The Message Will be Sent to ${email}`)
+    res.send(`The Message Will be Sent to ${email}`);
   } catch (err) {
     console.log("Error" + err);
-  } 
-})
+  }
+});
 
 var port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Successfully connected to Servere ${port}`))
-
-
-
+app.listen(port, () =>
+  console.log(`Successfully connected to Servere ${port}`)
+);
