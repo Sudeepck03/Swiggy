372aac52bf4255d801fdfff2731dfcc511b59ce9
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-04-19 07:45:12 +0530

    Update getRestuarntsLinks.js

--- getRestuarntsLinks.js
+++ getRestuarntsLinks.js
@@ -15,7 +15,7 @@ async function getRestuarntsLinks(city, restaurants) {
 
    await page.goto(`https://www.swiggy.com/city/bangalore`);
 
-  while (i < 4) {
+  while (i < 3) {
     await page.$eval(
       ".RestaurantList__ShowMoreContainer-sc-1d3nl43-0.ikntVJ > div > div:nth-child(1)",
       (el) => el.click()
